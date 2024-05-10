6432e908194201a33cb642feb661bb939cefa43c
Chukwuka Okonkwo David <chukwukaokonkwo99@gmail.com>
2024-05-10 08:07:18 +0100

    all features working successfully

--- src/components/LocationHistory.jsx
+++ src/components/LocationHistory.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import { supabase } from "../config/supabaseClient"
 
 const LocationHistory = () => {
+
+    const [locationHistory, setLocationHistory] = useState([])
+
+    useEffect(() => {
+        // Fetch location history data from Supabase when the component mounts
+        fetchLocationHistory();
+      }, []);
+
+    const fetchLocationHistory = async () => {
+        try {
+            // Fetch location history data from the 'location-history' table
+            let { data, error } = await supabase
+            .from('location-history')
+            .select('*')
+
+            if (error) throw error
+
+            // Set the fetched location history data in state
+            setLocationHistory(data)
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
   return (
     <div className="container mx-auto my-10 p-8 bg-white shadow-lg rounded-lg">
         <div>
@@ -14,11 +40,13 @@ const LocationHistory = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td className="border px-4 py-2"></td>
-                    <td className="border px-4 py-2"></td>
-                    <td className="border px-4 py-2"></td>
+                {locationHistory.map((location, index) => (
+                <tr key={index}>
+                    <td className="border px-4 py-2">{location.lat}</td>
+                    <td className="border px-4 py-2">{location.lng}</td>
+                    <td className="border px-4 py-2">{location.created_at}</td>
                 </tr>
+                ))}
             </tbody>
             </table>
         </div>
