60c8f2bf02a2a7fa426fcd5eff1558779c28ea38
Chukwuka Okonkwo David <chukwukaokonkwo99@gmail.com>
2024-05-14 09:14:21 +0100

    implemented lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
-import Sidebar from "./components/Sidebar";
-import Profile from "./components/Profile";
-import LocationHistory from "./components/LocationHistory";
-import MapLocation from "./components/MapLocation";
-import { useEffect, useState } from "react";
+import { Suspense, lazy, useEffect, useState } from "react";
+// import Login from "./Login";
+// import Register from "./Register";
+// import Sidebar from "./components/Sidebar";
+// import Profile from "./components/Profile";
+// import LocationHistory from "./components/LocationHistory";
+// import MapLocation from "./components/MapLocation";
+
+const Login = lazy(() => import("./Login"))
+const Register = lazy(() => import("./Register"))
+const Sidebar = lazy(() => import("./components/Sidebar"))
+const Profile = lazy(() => import("./components/Profile"))
+const LocationHistory = lazy(() => import("./components/LocationHistory"))
+const MapLocation = lazy(() => import("./components/MapLocation"))
 
 
 export default function App() {
@@ -25,8 +32,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login setToken={setToken}/>} />
-        <Route path="/registration" element={<Register />} />
+        <Route path="/" element={
+        <Suspense fallback={<div>loading...</div>}>
+          <Login setToken={setToken}/>
+        </Suspense>} />
+
+        <Route path="/registration" element={
+        <Suspense fallback={<div>loading...</div>}>
+          <Register />
+        </Suspense>} />
+
         <Route
           element={
             <div className="flex font-mono">
@@ -35,9 +50,20 @@ export default function App() {
             </div>
           }
         >
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/location-history" element={<LocationHistory />} />
-          {token ? <Route path="/map-location" element={<MapLocation token={token} />} /> : ""}
+          <Route path="/profile" element={
+          <Suspense fallback={<div>loading...</div>}>
+            <Profile />
+          </Suspense>} />
+
+          <Route path="/location-history" element={
+          <Suspense fallback={<div>loading...</div>}>
+            <LocationHistory />
+          </Suspense>} />
+          
+          {token ? <Route path="/map-location" element={
+          <Suspense fallback={<div>loading...</div>}>
+            <MapLocation token={token} />
+          </Suspense>} /> : ""}
         </Route>
       </Routes>
     </BrowserRouter>
